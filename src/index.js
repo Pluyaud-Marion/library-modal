ea943d10e92ddaa7f6fd10507111c7e31798742a
Marion Pluyaud <marion070189@hotmail.fr>
2022-08-18 18:27:00 +0200

    add toogle

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Modal } from './lib';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <Modal />
   </React.StrictMode>
 );
 
--- src/lib/components/Modal.js
+++ src/lib/components/Modal.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import "../components/styles/modal.css"
 
-const Modal = ({ contentModal }) => {
+const Modal = ({ contentModal, toogle }) => {
     return (
         <div className="modal">
             <span className="text">{contentModal}</span>
-            <span className="close-modal" >X</span>
+            <span className="close-modal" onClick={toogle} >X</span>
         </div>
     );
 };
