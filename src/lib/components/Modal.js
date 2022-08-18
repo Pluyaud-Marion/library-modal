8356c15b08421622596e3e87f13c65f5b08bd917
Marion Pluyaud <marion070189@hotmail.fr>
2022-08-18 18:37:05 +0200

    update toogle

--- src/lib/components/Modal.js
+++ src/lib/components/Modal.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import "../components/styles/modal.css"
 
-const Modal = ({ contentModal, toogle }) => {
+const Modal = ({ contentModal, hide }) => {
     return (
         <div className="modal">
             <span className="text">{contentModal}</span>
-            <span className="close-modal" onClick={toogle} >X</span>
+            <span className="close-modal" onClick={hide} >X</span>
         </div>
     );
 };
